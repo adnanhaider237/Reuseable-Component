d23cfd117e695d45160e9a27193579195bd3431b
adnanhaider237 <adnanhaider237@gmail.com>
2022-05-20 16:05:09 +0500

    init1

--- app/src/main/java/com/bottlestudio/reuseablecomponent/MainActivity.java
+++ app/src/main/java/com/bottlestudio/reuseablecomponent/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         MyToast.showToast(this, "Message");
-        //gekkdd
+        //gekkddasdf
+
     }
 }
